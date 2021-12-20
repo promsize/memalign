0089cc64c263a3a45fd39092e48deb936fabae6b
Ingo Albrecht <commit@promovicz.org>
2021-12-20 16:33:05 +0100

    Indent

--- memalign.h
+++ memalign.h
@@ -13,15 +13,15 @@
     ptralign(__alignof__(_type), sizeof(_type), _ptr_ref, _ptr_space)
 
 static inline void *ptralign(size_t align, size_t size, void **ptr, size_t space) {
-	const uintptr_t uptr = (uintptr_t)ptr;
-	const uintptr_t aligned = (uptr - 1u + align) & -align;
-	const uintptr_t diff = aligned - uptr;
-	if ((size + diff) > space)
-		return NULL;
-	else {
-		space -= diff;
-		return *ptr = (void*)aligned;
-	}
+  const uintptr_t uptr = (uintptr_t)ptr;
+  const uintptr_t aligned = (uptr - 1u + align) & -align;
+  const uintptr_t diff = aligned - uptr;
+  if ((size + diff) > space)
+    return NULL;
+  else {
+    space -= diff;
+    return *ptr = (void*)aligned;
+  }
 }
 
 #endif /* !MEMALIGN_H */
