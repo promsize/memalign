cc5000eb678a13d9a3aa341ff9a328f3d420bd5d
Ingo Albrecht <commit@promovicz.org>
2022-07-09 13:24:57 +0200

    More variants

--- memalign.h
+++ memalign.h
@@ -12,16 +12,23 @@
 #define PTRALIGN(_type, _ptr_ref, _ptr_space) \
     ptralign(__alignof__(_type), sizeof(_type), _ptr_ref, _ptr_space)
 
-static inline void *ptralign(size_t align, size_t size, void **ptr, size_t space) {
-  const uintptr_t uptr = (uintptr_t)ptr;
-  const uintptr_t aligned = (uptr - 1u + align) & -align;
-  const uintptr_t diff = aligned - uptr;
-  if ((size + diff) > space)
-    return NULL;
+static inline uintptr_t memalign(size_t align, size_t size, uintptr_t value, size_t space) {
+  const uintptr_t aligned = (value - 1u + align) & -align;
+  const uintptr_t delta = aligned - value;
+  if ((size + delta) > space)
+    return 0;
   else {
-    space -= diff;
-    return *ptr = (void*)aligned;
+    space -= delta;
+    return aligned;
   }
+}
+
+static inline void *ptralign(size_t align, size_t size, void *value, size_t space) {
+  return (void*)memalign(align, size, (uintptr_t)value, space);
+}
+
+static inline size_t sizealign(size_t align, size_t size, size_t value, size_t space) {
+  return (size_t)memalign(align, size, (uintptr_t)value, space);
 }
 
 #endif /* !MEMALIGN_H */
