88b34abdadeba5994f096a9ef49cff9c53c05ea1
Ingo Albrecht <commit@promovicz.org>
2022-07-09 13:25:08 +0200

    More tests

--- test.c
+++ test.c
@@ -8,24 +8,31 @@
 #include "memalign.h"
 
 int main(void) {
-  void *p;
+  char *p;
   uint8_t u8;
   uint16_t u16;
   uint32_t u32;
   uint64_t u64;
 
-  p = &u8;
+  p = (char *)&u8;
   assert(PTRALIGN(uint8_t, p, sizeof(u8)) == p);
   assert(PTRALIGN(uint8_t, p, sizeof(u8) - 1) == NULL);
-  p = &u16;
+  p = (char *)&u16;
   assert(PTRALIGN(uint16_t, p, sizeof(u16)) == p);
   assert(PTRALIGN(uint16_t, p, sizeof(u16) - 1) == NULL);
-  p = &u32;
+  assert(PTRALIGN(uint16_t, p - 1, sizeof(u16) + 1) == p);
+  p = (char *)&u32;
   assert(PTRALIGN(uint32_t, p, sizeof(u32)) == p);
   assert(PTRALIGN(uint32_t, p, sizeof(u32) - 1) == NULL);
-  p = &u64;
+  assert(PTRALIGN(uint32_t, p - 1, sizeof(u32) + 1) == p);
+  assert(PTRALIGN(uint32_t, p - 2, sizeof(u32) + 2) == p);
+  assert(PTRALIGN(uint32_t, p - 3, sizeof(u32) + 3) == p);
+  p = (char *)&u64;
   assert(PTRALIGN(uint64_t, p, sizeof(u64)) == p);
   assert(PTRALIGN(uint64_t, p, sizeof(u64) - 1) == NULL);
+  assert(PTRALIGN(uint64_t, p - 1, sizeof(u64) + 1) == p);
+  assert(PTRALIGN(uint64_t, p - 2, sizeof(u64) + 2) == p);
+  assert(PTRALIGN(uint64_t, p - 3, sizeof(u64) + 3) == p);
 
   puts("All tests succeeded.");
 
